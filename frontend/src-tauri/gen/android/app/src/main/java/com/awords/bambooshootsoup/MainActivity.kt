3128c7b3baf28446921ed4d4e4694612295097a6
A-Words <awordsgg@outlook.com>
2026-02-24 13:42:15 +0800

    feat: 初始化 Android Project 结构与资源

---
+++ frontend/src-tauri/gen/android/app/src/main/java/com/awords/bambooshootsoup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.awords.bambooshootsoup
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
